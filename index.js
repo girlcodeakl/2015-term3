8eb31a2222e285f566bccb6e24eb2ce9cc0a783f
miallewellyn <miamoollewellyn@gmail.com>
2015-08-27 20:23:18 +1200

    added login stuff

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express = require('express')
 var app = express();
 var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser');
 
 //If a client asks for a file,
 //look in the public folder. If it's there, give it to them.
@@ -10,6 +11,8 @@ app.use(express.static(__dirname + '/public'));
 //this lets us read POST data
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+var bodyParser = require('body-parser');
+app.use(cookieParser())
 
 //make an empty list of ideas
 var coolIdeas = [];
@@ -25,6 +28,12 @@ app.post('/ideas', function (req, res) {
   console.log(req.body.idea); //write it on the command prompt so we can see
   coolIdeas.push(req.body.idea); //save a new idea
   res.send("thanks for your idea");
+});
+
+app.post('/Login', function (req, res) {
+  var userName = "alice";
+res.cookie('name', userName, { maxAge: 900000, httpOnly: true });
+  console.log(userName); //write it on the command prompt so we can see
 });
 
 //listen for connections on port 3000
